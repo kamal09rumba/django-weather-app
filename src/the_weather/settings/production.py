516d4b3f146927d079dbc9467034bb27a66937ae
Kamal Lama <kamal09rumba@gmail.com>
2019-06-06 14:53:57 +0545

    debug toolbar setup

---
+++ src/the_weather/settings/production.py
@@ -0,0 +1,22 @@
+from .base from *
+
+DEBUG = False
+ALLOWED_HOSTS = ['localhost']
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator', },
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator', },
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_name',
+        'USER': 'db_user',
+        'PASSWORD': 'db_pass',
+        'HOST': '',
+        'PORT': ''
+    }
+}
